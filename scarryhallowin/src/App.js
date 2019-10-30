c532e1ab6c8e4dd971a4c46a329ef23cd73d42ee
pablo <pmo83.mp@gmail.com>
2019-10-30 17:20:01 +0100

    esquisse de la movieCard

--- scarryhallowin/src/App.js
+++ scarryhallowin/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MovieCards from './Components/MovieCards';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCards/>
+      <MovieCards/>
+      <MovieCards/>
+      <MovieCards/>
+      <MovieCards/>
     </div>
   );
 }
